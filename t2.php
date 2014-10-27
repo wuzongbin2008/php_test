16092a44a2daa9bc768caad0e06cef482ffe70f1
wujiang1 <wujiang1@staff.sina.com.cn>
2014-10-27 17:09:02 +0800

    up

--- t2.php
+++ t2.php
@@ -1,6 +1,21 @@
 <?php
-$a = array(180, 50, 30);
+$info = array(
+    'version'	=>	1,
+    'callbacks' => 1,
 
-foreach($a as $v)
-    var_dump($v);
+    'param'		=>	array(
+        'uid'		=>	222,
+        'pid'		=>	333,
+        'count'		=>	1,
+        'size'		=>	100,
+        'height'	=>	55,
+        'length'	=>	66,
+        'format'	=>	1,
+        'exif'		=>	".jpg",
+        'ip'		=>	"127.0.0.1",
+        'appid'		=>	5,
+    ),
+);
+
+echo json_encode($info);
 ?>
