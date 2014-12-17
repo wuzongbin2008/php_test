20cf7a07fd059ea4d2f5f65a42b07df792cb5450
wujiang1 <wujiang1@staff.sina.com.cn>
2014-12-17 15:19:47 +0800

    add files

---
+++ hash/consistent_hash.php
@@ -0,0 +1,155 @@
+<?php
+/******************************************************
+ * A implementation of consistent hashing
+ *****************************************************/
+class CHASH {
+        private		$nodes;		//aaray
+        private		$pos;		//array
+        private		$ok;		//bool
+
+        private		$ptr;
+
+        /**
+         * Constructor
+         * @param Array $config array of candiddate nodes, unrelated with elements' order
+         * @param int $replics of a real node
+         */
+        function __construct($config, $replics=503)
+        {
+                $this->ptr = -1;
+                $this->pos = array();
+
+                $this->nodes = $config;
+                $this->ok = true;
+                if ($replics < 1) $this->ok = false;
+                if (empty($config) || !is_array($config) || count($config) < 1)
+                        $this->ok = false;
+
+                //debug
+                $exist = 0;
+                $pos_arr = array();
+                $exist_arr = array();
+                //end debug
+
+                foreach ($config as $k => $v) {
+                        for ($i=0; $i<$replics; $i++) {
+                                $str = $v.":".$i;
+                                $hash = $this->_hash($str);
+
+                                //debug
+                                if(isset($this->pos[$hash])) {
+                                        $exist++;
+                                        $exist_arr[] = array(
+                                                "hash" =>$hash,
+                                                "old" =>$pos_arr[$hash]['str'],
+                                                "new" =>$str
+                                        );
+                                }
+                                $pos_arr[$hash] = array(
+                                        "k" =>$k,
+                                        "str" =>$str
+                                );
+                                //end debug
+
+                                $this->pos[$hash] = $k;
+                        }
+                }
+                //echo "exist: $exist\n";
+                //print_r($exist_arr);
+                ksort($this->pos, SORT_NUMERIC);
+        }
+
+        function __destruct()
+        {
+                unset($this->nodes);
+                unset($this->pos);
+                unset($this->ok);
+        }
+
+        /**
+         * get the first node by key
+         * @param string $key
+         * @return mix real node
+         */
+        function get_node($key)
+        {
+                if (!$this->ok) return false;
+
+                $hash = $this->_hash($key);
+                $this->ptr = $pos = $this->_find_pos($hash);
+
+                return $this->nodes[$this->pos[$pos]];
+        }
+
+        function get_nodes($key, $n)
+        {
+                if (!$this->ok) return false;
+                if ($n > $this->nodes) return false;
+                //echo "key: $key\n";
+
+                $tmp = array();
+
+                $pos = $this->_hash($key);
+
+                while ($n) {
+                        $pos = $this->_find_pos($pos);
+                        //echo "pos: $pos\t";
+                        //echo "pos[pos]:".$this->pos[$pos]."\n";
+                        if (in_array($this->pos[$pos], $tmp)) {
+                                $pos += 1;
+                                continue;
+                        }
+                        array_push($tmp, $this->pos[$pos]);
+                        $n--;
+                        $this->ptr = $pos;
+                        $pos += 1;
+                }
+                echo "tmp: ";print_r($tmp);
+                $res = array();
+                foreach ($tmp as $pos) {
+                        array_push($res, $this->nodes[$pos]);
+                }
+
+                return $res;
+        }
+
+        /**
+         * get the substitute node
+         * @return mix real node
+         */
+        function next_node()
+        {
+                if (!$this->ok) return false;
+                if ($this->ptr == -1) return false;
+
+                $pos = $this->ptr;
+                do {
+                        $pos = $this->_find_pos($pos+1);
+                } while ($this->pos[$pos] == $this->pos[$this->ptr]);
+
+                $this->ptr = $pos;
+                return $this->nodes[$this->pos[$pos]];
+
+        }
+
+        function _find_pos($hash)
+        {
+                foreach ($this->pos as $k => $v) {
+                        if ($k >= $hash) return $k;
+                }
+
+                foreach ($this->pos as $k => $v) {
+                        return $k;
+                }
+
+                //never to here
+                return 0;
+        }
+
+        function _hash($str)
+        {
+                $hash_v = (crc32($str) & 0x7fffffff) % 50119;
+                //echo "_hash str: $str\thash_v: $hash_v\n";
+                return $hash_v;
+        }
+}
---
+++ hash/t.php
@@ -0,0 +1,46 @@
+<?php
+include("consistent_hash.php");
+
+$slice_icache_servers_1 = array('10.13.1.153:8081', '10.13.1.154:8081', '10.13.1.155:8081', '10.13.1.156:8081');
+$slice_icache_servers_0 = array('10.13.1.157:8081', '10.13.1.158:8081', '10.13.1.159:8081', '10.13.1.160:8081');
+$cluster = array($slice_icache_servers_0, $slice_icache_servers_1);
+$retry_times = 3;
+$token = "2183098411.9af8565bc6c28f082b4cadd965727262.713062.240.1418111810.pic";
+
+$hash_v = (crc32($token) & 0x7fffffff) % 50119;
+//echo $hash_v."\n";
+
+$rings = array();
+foreach($cluster as $ring) {
+        $ch = new CHASH($ring);
+        array_push($rings, $ch);
+        break;
+}
+
+foreach ($rings as $ring) {
+
+        $ret = false;
+
+        $host_list = $ring->get_nodes($token, $retry_times);
+        if (!$host_list) {
+                return false;
+        }
+        echo "host_list:";print_r($host_list);exit;
+        foreach ($host_list as $k => $host) {
+                echo("k: $k\tset host: {$host}\n");
+                /*$ret = $this->post_data($url, $host, $blob);
+                if ($ret) {
+                        $ret = true;
+                        break;
+                }*/
+        }
+}
+
+
+$c = array('10.13.1.153:8081', '10.13.1.154:8081', '10.13.1.155:8081', '10.13.1.156:8081');
+if($retry_times > $c){
+        echo "more >";
+}else{
+        echo "less <";
+}
+exit;
