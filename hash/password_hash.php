080c61ccfc85fb01b05d3dcac11bb5861ef22adb
wujiang1 <wujiang1@staff.sina.com.cn>
2015-03-04 11:19:32 +0800

    hash test

---
+++ hash/password_hash.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wj
+ * Date: 15-3-3
+ * Time: ÏÂÎç3:56
+ */
+
+$str = "123";
+
+$h = password_hash($str, PASSWORD_DEFAULT);
+$h2 = password_hash($str, PASSWORD_BCRYPT);
+//print_r(password_get_info($h));
+printf("h: %s\nlen: %d\n", $h, strlen($h));
+printf("h2: %s\nlen: %d\n", $h2, strlen($h2));
+
+if(password_verify($str, $h)){
+	echo "equal\n";
+}else{
+	echo "not equal\n";
+}
